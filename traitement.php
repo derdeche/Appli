c44f0cd968c0303deb66d74b82ee597f9aff777f
derdeche <foued.derdeche@gmail.com>
2023-07-19 16:22:36 +0200

    upload immage

--- traitement.php
+++ traitement.php
@@ -15,14 +15,55 @@ switch ($_GET['action']){
                 "name" =>$name,
                 "price"=> $price,
                 "qtt" =>$qtt,
-                "total" => $price*$qtt
+                "total" => $price*$qtt,
+                "image"=>$name,
+
                         ];
 
         $_SESSION['products'][] = $product;
                 
             }
         }
+        /*on vérifie si l’index « file » existe*/
+        if(isset($_FILES['file'] )&& !empty($_FILES["image"])){
+            $tmpName = $_FILES['file']['tmp_name'];
+            $name = $_FILES['file']['name'];
+            $size = $_FILES['file']['size'];
+            $error = $_FILES['file']['error'];
+            $type = $_FILES["file"]["type"];
+            
+            $tabExtension = explode('.', $name);
+            // move_uploaded_file($tmpName, './upload/'.$name);
+            $extension = strtolower(end($tabExtension));
+            //Tableau des extensions que l'on accepte
+            
 
+            if(in_array($extension, $extensions))
+                {
+             move_uploaded_file($tmpName, './upload/'.$name);
+                }
+            else{
+                    echo "Mauvaise extension";
+                }
+
+        }
+
+        //Vérifier s’il y a une erreur
+        $extensions = ['jpg', 'png', 'jpeg', 'gif'];
+            $maxSize = 400000;
+        if(in_array($extension, $extensions) && $size <= $maxSize && $error == 0){
+            move_uploaded_file($tmpName, './upload/'.$name);
+        }
+        else{
+            echo "Une erreur est survenue";
+        }
+
+        if(in_array($extension, $extensions) && $size <= $maxSize){
+            move_uploaded_file($tmpName, './upload/'.$name);
+        }
+        else{
+            echo "Mauvaise extension ou taille trop grande";
+        }
 
         header("Location:index.php"); 
 		break;
@@ -58,4 +99,4 @@ switch ($_GET['action']){
                 }
                     break;
 		}
-
+        require_once "upload";
